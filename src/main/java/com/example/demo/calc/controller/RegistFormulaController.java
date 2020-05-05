efd4e8871e91d43c64573a2e9128c1563565f13e
yTakaki <yakkosan716@gmail.com>
2020-05-05 22:44:53 +0900

    add insert method to RegistFormulaController

--- src/main/java/com/example/demo/calc/controller/RegistFormulaController.java
+++ src/main/java/com/example/demo/calc/controller/RegistFormulaController.java
@@ -1,5 +1,6 @@
 package com.example.demo.calc.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -8,11 +9,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.calc.domain.model.Formula;
 import com.example.demo.calc.domain.model.GroupOrder;
 import com.example.demo.calc.domain.model.RegistFormulaForm;
+import com.example.demo.calc.domain.service.FormulaService;
 
 @Controller
 public class RegistFormulaController {
+
+	@Autowired
+	private FormulaService formulaService;
 
 	@GetMapping("/regist")
 	public String getRegistFormula(@ModelAttribute RegistFormulaForm form,Model model) {
@@ -21,11 +27,29 @@ public class RegistFormulaController {
 	}
 
 	@PostMapping("/regist")
-	public String postRegistFormula(@ModelAttribute @Validated(GroupOrder.class) RegistFormulaForm form,BindingResult bindingResult,Model model) {
+	public String postRegistFormula(@ModelAttribute @Validated(GroupOrder.class) RegistFormulaForm form,
+			BindingResult bindingResult,Model model) {
 		if (bindingResult.hasErrors()) {
 			return getRegistFormula(form,model);
 		}
 		System.out.println(form);
+
+		Formula formula = new Formula();
+
+		formula.setFormulaId(form.getFormulaId());
+		formula.setFormulaName(form.getFormulaName());
+		formula.setValueYear(form.getValueYear());
+		formula.setValueMonth(form.getValueMonth());
+		formula.setValueDay(form.getValueDay());
+		formula.setDesignerDay(form.getDesignerDay());
+
+		boolean result = formulaService.insert(formula);
+		if (result==true) {
+			System.out.println("insert success.");
+		} else {
+			System.out.println("insert fault.");
+		}
+
 		return "redirect:/home";
 	}
 
