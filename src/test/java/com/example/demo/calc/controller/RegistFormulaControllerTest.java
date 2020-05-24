da8812090082b38f93b76fd8feb05c1a4ee1a874
yTakaki <yakkosan716@gmail.com>
2020-05-24 16:38:02 +0900

    add RegistFormulaControllerTest

--- src/test/java/com/example/demo/calc/controller/RegistFormulaControllerTest.java
+++ src/test/java/com/example/demo/calc/controller/RegistFormulaControllerTest.java
@@ -1,14 +1,15 @@
 package com.example.demo.calc.controller;
 
+import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -24,9 +25,17 @@ public class RegistFormulaControllerTest {
 		.andExpect(view().name("calc/registFormula"));
 	}
 
-	@Disabled
 	@Test
+	@Transactional // for test insert
 	void registへのPOSTリクエストに対するテスト() throws Exception {
-		this.mockMvc.perform(post("/regist"));
+		// execute and assertion
+		this.mockMvc.perform(post("/regist")
+				.param("formulaId","99999")
+				.param("formulaName","testdata")
+				.param("valueDay","1"))
+		.andExpect(model().hasNoErrors())
+		.andExpect(model().attribute("result",is("計算式を1件、追加しました。")))
+		.andExpect(view().name("calc/homeCalculator"));
+		//
 	}
 }
