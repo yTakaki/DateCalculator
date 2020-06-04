d485f2030bea2668ed94ce139b3a3a080aaea2a4
yTakaki <yakkosan716@gmail.com>
2020-06-04 17:09:56 +0900

    refactoring CalcServiceTest to nest class

--- src/test/java/com/example/demo/calc/domain/service/CalcServiceTest.java
+++ src/test/java/com/example/demo/calc/domain/service/CalcServiceTest.java
@@ -9,6 +9,7 @@ import java.time.DateTimeException;
 import java.time.LocalDate;
 import java.util.List;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -24,8 +25,7 @@ import com.example.demo.calc.domain.repository.FormulaMapper;
 public class CalcServiceTest {
 
 	@ComponentScan({
-		"com.example.demo.calc.domain.service",
-		"com.example.demo.calc.domain.repository"
+		"com.example.demo.calc.domain.service"
 	})
 	static class Config {
 	}
@@ -36,42 +36,50 @@ public class CalcServiceTest {
 	@MockBean
 	private FormulaMapper mapper;
 
-	@Test
-	void 計算基準日がNullのときNullPointerExceptionを返すこと() throws Exception {
-		Formula f = new Formula("99999","test",0,0,0,1);
-		when(mapper.selectAll()).thenReturn(List.of(f));
-		assertThrows(NullPointerException.class,() -> sut.calculation(null));
+	@Nested
+	class 例外処理 {
+		@Test
+		void 計算基準日がNullのときNullPointerExceptionを返すこと() throws Exception {
+			Formula f = new Formula("99999","test",0,0,0,1);
+			when(mapper.selectAll()).thenReturn(List.of(f));
+			assertThrows(NullPointerException.class,() -> sut.calculation(null));
+		}
+
+		@Test
+		void 計算基準日が不適切な日付のときDateTimeExceptionを返すこと() throws Exception {
+			assertThrows(DateTimeException.class,() -> sut.calculation(LocalDate.of(2020, 9, 31)));
+		}
 	}
 
-	@Test
-	void 計算基準日が不適切な日付のときDateTimeExceptionを返すこと() throws Exception {
-		assertThrows(DateTimeException.class,() -> sut.calculation(LocalDate.of(2020, 9, 31)));
+	@Nested
+	class 日付計算 {
+		@ParameterizedTest
+		@CsvSource(value = {
+				"2018-12-01, 0, 0, 0, 0, 2018-12-01", // すべて未指定(指定日なし）
+				"2018-12-01, 0, 0, 0, 20, 2018-12-20", // 日付指定(日付<指定日)
+				"2018-12-31, 0, 0, 0, 20, 2018-12-20", // 日付指定(日付>指定日)
+				"2018-12-01, 0, 0, 0, 29, 2018-12-31", // 末日指定(加算日なし)
+				"2018-12-01, 0, 0, -1, 29, 2018-12-31", // 末日指定(加算日あり)
+				"2018-12-01, 0, 0, 1, 0, 2018-12-02", // 翌日
+				"2018-12-01, 0, 0, -1, 0, 2018-11-30", // 前日
+				"2018-11-01, 0, 1, 0, 0, 2018-12-01", // 翌月
+				"2018-12-01, 0, -1, 0, 0, 2018-11-01", // 前月
+				"2018-12-01, 1, 0, 0, 0, 2019-12-01", // 翌年
+				"2018-12-01, -1, 0, 0, 0, 2017-12-01", // 前年
+				"2018-12-01, 1, 1, 1, 1, 2020-01-01", // すべて指定(指定日あり)
+				"2018-10-31, 0, 1, 0, 0, 2018-11-30", // 月の加算で日数の切り捨て
+				"2018-12-02, 0, 13, 0, 0, 2020-01-02", // 年月またぎ
+				"2018-12-02, 0, 0, 365, 0, 2019-12-02", // 年月またぎ
+		})
+		void test(LocalDate calcDate,int valueYear,int valueMonth,int valueDay,
+				int designerDay,LocalDate expect) throws Exception {
+			Formula f = new Formula("99999","test",valueYear,valueMonth,valueDay,designerDay);
+			when(mapper.selectAll()).thenReturn(List.of(f));
+			LocalDate actual = sut.calculation(calcDate).get(0).getResultDate();
+			assertThat(actual,is(expect));
+		}
 	}
 
-	@ParameterizedTest
-	@CsvSource(value = {
-			"2018-12-01, 0, 0, 0, 0, 2018-12-01", // すべて未指定(指定日なし）
-			"2018-12-01, 0, 0, 0, 20, 2018-12-20", // 日付指定(日付<指定日)
-			"2018-12-31, 0, 0, 0, 20, 2018-12-20", // 日付指定(日付>指定日)
-			"2018-12-01, 0, 0, 0, 29, 2018-12-31", // 末日指定(加算日なし)
-			"2018-12-01, 0, 0, -1, 29, 2018-12-31", // 末日指定(加算日あり)
-			"2018-12-01, 0, 0, 1, 0, 2018-12-02", // 翌日
-			"2018-12-01, 0, 0, -1, 0, 2018-11-30", // 前日
-			"2018-11-01, 0, 1, 0, 0, 2018-12-01", // 翌月
-			"2018-12-01, 0, -1, 0, 0, 2018-11-01", // 前月
-			"2018-12-01, 1, 0, 0, 0, 2019-12-01", // 翌年
-			"2018-12-01, -1, 0, 0, 0, 2017-12-01", // 前年
-			"2018-12-01, 1, 1, 1, 1, 2020-01-01", // すべて指定(指定日あり)
-			"2018-10-31, 0, 1, 0, 0, 2018-11-30", // 月の加算で日数の切り捨て
-			"2018-12-02, 0, 13, 0, 0, 2020-01-02", // 年月またぎ
-			"2018-12-02, 0, 0, 365, 0, 2019-12-02", // 年月またぎ
-	})
-	void 日付計算(LocalDate calcDate,int valueYear,int valueMonth,int valueDay,
-			int designerDay,LocalDate expect) throws Exception {
-		Formula f = new Formula("99999","test",valueYear,valueMonth,valueDay,designerDay);
-		when(mapper.selectAll()).thenReturn(List.of(f));
-		LocalDate actual = sut.calculation(calcDate).get(0).getResultDate();
-		assertThat(actual,is(expect));
-	}
+
 
 }
